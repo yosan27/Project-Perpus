f1050da4c9d7351ce9f35dbc985cd242266a20fc
yosan27 <73471678+yosan27@users.noreply.github.com>
2020-11-11 15:42:57 +0700

    menghubungkan best seller dengan detailpage

--- assets/js/detailClick.js
+++ assets/js/detailClick.js
@@ -5,8 +5,3 @@ slider.addEventListener('mouseenter', function(){
     open("detailpage.html", "_self");
   });
 });
-
-const bestSeller = document.querySelector('.body .best .ulBest li');
-bestSeller.addEventListener('click', function(){
-  console.log('Best Seller!');
-});
