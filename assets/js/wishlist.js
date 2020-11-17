b2a91c34ed4f1927eb41cfa8bf8bf1e67486a00e
Mochamad Dwi Fadly <fadlydwi48@gmail.com>
2020-11-17 09:27:18 +0700

    update menu tampil cart dan wishlist dan update nav user

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -13,3 +13,13 @@ btnPinjam2.addEventListener("click", function () {
     document.getElementById("cartCount").innerHTML = "3";
 
 });
+
+var btndata1 = document.getElementById("btn-data1");
+btndata1.addEventListener("click", function () {
+    document.getElementById("data1").style.display = "none";
+});
+
+var btndata2 = document.getElementById("btn-data2");
+btndata2.addEventListener("click", function () {
+    document.getElementById("data2").style.display = "none";
+});
